0405f4a55b5ea4a02ac797511b904ce6573d5a98
Fernanda <mimisamp1503@gmail.com>
2026-06-02 22:25:30 -0300

    Terminei a Entity (quase na verdade, to esperando quem vai fazer a classe Animal e só isso) e fiz o Enum que eu usei como atributo pra Enity.

--- src/main/java/com/adoptme/api/formulario_adocao/model/FormularioAdocao.java
+++ src/main/java/com/adoptme/api/formulario_adocao/model/FormularioAdocao.java
@@ -1,8 +1,50 @@
 package com.adoptme.api.formulario_adocao.model;
 
-import jakarta.persistence.Entity;
+import com.adoptme.api.formulario_adocao.enums.StatusSolicitacao;
+import com.adoptme.api.usuario.domain.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 
+@Table(name = "tb_formularioAdocao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class FormularioAdocao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Usuario usuario;
+
+    //Aqui ficaria o atributo de animais, porém eu to esperando o caba fazer a classe.
+    // Vai levar o ManyToOne também e o nullable = false gg ezz papai
+
+    @Enumerated(EnumType.STRING)
+    private StatusSolicitacao status = StatusSolicitacao.PENDENTE;
+
+    private String justificativaStatus;
+
+    @JoinColumn(nullable = false)
+    private Boolean temOutrosAnimais;
+
+    @JoinColumn(nullable = false)
+    private String tipoResidencia;
+
+    @JoinColumn(nullable = false)
+    private LocalDateTime dataSolicitacao;
+
+    @PrePersist
+    protected void onCreate() {
+        this.dataSolicitacao = LocalDateTime.now();
+        this.status = StatusSolicitacao.PENDENTE;
+    }
+
 }
